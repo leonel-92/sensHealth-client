1f08691436c49511d3f786398268da9d685502dd
Jorge Leonel Sánchez Fregozo <leo.metallica17@gmail.com>
2016-11-24 13:51:52 -0700

    Clean code and delete "console.log()"

--- worker.js
+++ worker.js
@@ -2,31 +2,19 @@ function timedCount() {
 
 self.addEventListener('message',function (e) {
 	var coor = e.data;
-	console.log("Datos: "+ coor);
 	//Envío de datos al servidor
 	var valores = JSON.parse(coor);
     var estado;
-	console.log("valores: "+valores.cliente);
-	console.log("frecuencia: "+valores.frecuencia);
-	console.log("posicion: "+valores.posicion.lat+"  "+valores.posicion.lon);
-
-
 
 var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
-    console.log('status de peticion inicializacion: '+xmlhttp.readyState);
 xmlhttp.open("POST", "http://localhost:8080/posiciones");
-    console.log('status de peticion puerto abierto: '+xmlhttp.readyState);
 xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    console.log('status de peticion header: '+xmlhttp.readyState);
 
     xmlhttp.onprogress = function () {
-    console.log('LOADING', xmlhttp.readyState); // readyState will be 3
 };
     xmlhttp.onload = function () {
-    console.log('DONE', xmlhttp.readyState); // readyState will be 4
         estado =  xmlhttp.readyState;
         if(estado==4){
-            console.log('status de estado: '+estado);
             postMessage(estado);
         }else{
 
@@ -39,15 +27,8 @@ xmlhttp.send(JSON.stringify({cliente: valores.cliente,
         fechalocal: valores.fechalocal
 
       }));
-    console.log('status de peticion enviado: '+xmlhttp.readyState);
-    console.log('Datos guardados en base de datos satisfactoriamente');
-
-
-
 
 },false);
-
-
 
 	setTimeout("timedCount()",10000);
 }
